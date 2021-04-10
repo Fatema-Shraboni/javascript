2257ea6f143a28b5d90bc09d7dfbdabfc7ae9703
Kaniz Fatema <77629494+Fatema-Shraboni@users.noreply.github.com>
2021-04-10 19:44:14 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+function textChange(){
+  		// statement...
+      document.getElementById('h2').innerHTML ="Hello There";
+
+      if (new Date().getHours() < 18) {
+     document.getElementById("demo").innerHTML = "Good day!";
+}
+  	}
+
+
